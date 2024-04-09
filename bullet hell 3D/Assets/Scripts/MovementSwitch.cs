2492b5225e8753455982e66d7423ace9188ead7f
LusseveldNiek <97088465@st.deltion.nl>
2024-04-09 22:41:56 +0200

    movement

---
+++ bullet hell 3D/Assets/Scripts/MovementSwitch.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementSwitch : MonoBehaviour
+{
+    [Header("CamPosition")]
+    public Transform camPosition;
+
+    public Transform twoDPosition;
+    public Transform threeDPosition;
+
+    [Header("2DMovement")]
+    public float forwardSpeed;
+    public float sideSpeed;
+
+    [Header("3DMovement")]
+    public Transform lookPosition;
+    public float moveSpeed;
+
+    public float maxYRotation;
+    public float maxZRotation;
+
+    public bool inGang;
+    void Update()
+    {
+        if(inGang)
+        {
+            //move sideways
+            float sideMovement = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * sideMovement * sideSpeed * Time.deltaTime);
+
+            //forward
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                float forwardMovement = Input.GetAxis("Vertical");
+                transform.Translate(transform.forward * forwardMovement * forwardSpeed * Time.deltaTime);
+            }
+
+            //down and up
+            else
+            {
+                float upMovement = Input.GetAxis("Vertical");
+                transform.Translate(Vector3.up * upMovement * sideSpeed * Time.deltaTime);
+            }
+
+        }
+
+        else
+        {
+            //get the mouse position
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 4;
+            Vector3 targetPos = Camera.main.ScreenToWorldPoint(mousePos);
+            lookPosition.position = targetPos;
+
+            //look at mouse position
+            transform.LookAt(targetPos);
+
+            //limit rotation
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            float clampedYRotation = Mathf.Clamp(currentRotation.y, -maxYRotation, maxYRotation);
+            float clampedZRotation = Mathf.Clamp(currentRotation.z, -maxZRotation, maxZRotation);
+            transform.rotation = Quaternion.Euler(currentRotation.x, clampedYRotation, clampedZRotation);
+
+            //move forward and backwards
+            float forward = Input.GetAxis("Vertical");
+            transform.Translate(transform.forward * forward * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "gang")
+        {
+            inGang = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "gang")
+        {
+            inGang = false;
+        }
+    }
+}
