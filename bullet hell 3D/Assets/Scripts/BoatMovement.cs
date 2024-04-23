2f400e827044434560cdd6101191aef46035ff1c
LusseveldNiek <97088465@st.deltion.nl>
2024-04-23 20:38:38 +0200

    overworld movement

---
+++ bullet hell 3D/Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        float horizontalSpeed = Input.GetAxis("Horizontal");
+        float verticalSpeed = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody>().AddForce(-Vector3.forward * speed * verticalSpeed * Time.deltaTime);
+
+        GetComponent<Rigidbody>().AddForce(-Vector3.right * speed * horizontalSpeed * Time.deltaTime);
+    }
+}
