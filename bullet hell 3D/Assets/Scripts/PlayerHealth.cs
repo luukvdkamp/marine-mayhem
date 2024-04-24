3370a21f3d5393b465a510eaae4b0b8a5ffe38ff
LusseveldNiek <97088465@st.deltion.nl>
2024-04-25 00:03:32 +0200

    death transition

--- bullet hell 3D/Assets/Scripts/PlayerHealth.cs
+++ bullet hell 3D/Assets/Scripts/PlayerHealth.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public Image healthImage;
-    public TextMeshProUGUI textNumber;
     public int textInt;
     public float health;
 
@@ -16,7 +15,19 @@ public class PlayerHealth : MonoBehaviour
     public float resetTime;
     private float resetCounter;
 
+    [Header("UI")]
+    public Image healthImage;
+    public TextMeshProUGUI textNumber;
+    public Image deathImage;
+
+    [Header("Audio")]
     public AudioSource ouch;
+    public AudioSource dead;
+
+    [Header("GameOver")]
+    public float fadeDuration; // Duration of the fade effect
+    private float fadeTimer = 0f;
+
     void Update()
     {
         textNumber.text = textInt.ToString();
@@ -24,15 +35,24 @@ public class PlayerHealth : MonoBehaviour
 
         resetCounter += Time.deltaTime;
 
+        //if hit
         if(hit)
         {
             health -= 0.25f;
             textInt--;
 
-            ouch.Play();
+            if(textInt != 0)
+            {
+                ouch.Play();
+            }
 
             resetCounter = 0;
             hit = false;
+        }
+
+        if (textInt <= 0)
+        {
+            StartCoroutine(DeathTransition());
         }
     }
 
@@ -45,5 +65,20 @@ public class PlayerHealth : MonoBehaviour
                 hit = true;
             }
         }
+    }
+
+    //UX
+    IEnumerator DeathTransition()
+    {
+        //dead.Play();
+        while (fadeTimer < fadeDuration)
+        {
+            fadeTimer += Time.deltaTime;
+            float alpha = fadeTimer / fadeDuration;
+            deathImage.color = new Color(0f, 0f, 0f, alpha);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(0);
     }
 }
