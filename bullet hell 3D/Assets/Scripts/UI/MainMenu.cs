ccb449826968c7d9fee0362d7f899b963b398833
LusseveldNiek <97088465@st.deltion.nl>
2024-07-02 12:25:27 +0200

    main menu fixed

---
+++ bullet hell 3D/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        
+    }
+}
